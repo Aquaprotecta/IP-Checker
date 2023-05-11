03ec2dbb9a3d1f369506a188d8dff5105c86639e
Aquaprotecta <239863@edu.rocfriesepoort.nl>
2023-05-11 11:45:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 import requests
 
-    
 def request(address):
     url = "https://api.api-ninjas.com/v1/iplookup?address={}".format(address)
     response = requests.get(url, headers={'X-Api-Key': 'm/kt1fTkQods5DwclGVhYg==y1SfZTmIJZLImGQG'})
     if response.status_code == 200:
         return response.json()
     else:
-        return print("Error:", response.status_code, response.text) #TBD
-    
-response = request("1.1.1.1")
-print(response)
+        print("Error:", response.status_code, response.text)
+        return None
+
+address = input("Enter a valid IP address: ")
+response_data = request(address)
+
+if response_data:
+    print(response_data)
+
+os.system("pause")
